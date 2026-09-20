435dfbeec8eb6f59e427295d60cadd96afa928a8
JustPlanB <sepehr.g66@gmail.com>
2026-09-20 20:22:17 +0330

    Add files via upload

--- AppLockPlugin.java
+++ AppLockPlugin.java
@@ -180,6 +180,7 @@ public class AppLockPlugin extends Plugin {
               .setSubtitle(subtitle != null ? subtitle : "")
               .setNegativeButtonText("انصراف")
               .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_WEAK)
+              .setConfirmationRequired(false)
               .build();
             prompt.authenticate(info);
           } catch (Exception e) {
